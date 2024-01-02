80d9751964e3e2779b825dd4dce97e87805a2896
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2024-01-02 13:23:42 +0200

    3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
@@ -14,6 +14,13 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    
+    res.status(204).end()
 })
 
 app.get('/api/persons/:id', (req, res) => {
