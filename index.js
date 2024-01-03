b2336d0e6ccd42a6d90f0f652387ae008080f995
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2024-01-03 16:49:49 +0200

    3.10

--- index.js
+++ index.js
@@ -69,5 +69,7 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
-const PORT = 3001
-app.listen(PORT)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
